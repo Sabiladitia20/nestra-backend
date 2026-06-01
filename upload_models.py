0269244cc392a2f2afbb2e075177ebb755dad277
Sabil Aditia <sabiladitia20@gmail.com>
2026-06-01 10:48:42 +0700

    Load models from HuggingFace, remove joblib from repo

---
+++ upload_models.py
@@ -0,0 +1,22 @@
+﻿from huggingface_hub import HfApi
+
+api = HfApi()
+models = [
+    "model_baron",
+    "model_bawean",
+    "model_pandeglang",
+    "model_situbondo",
+    "model_sukabumi"
+]
+
+for model in models:
+    print(f"Uploading {model}...")
+    api.upload_file(
+        path_or_fileobj=f"pltb_artifacts/models/{model}.joblib",
+        path_in_repo=f"{model}.joblib",
+        repo_id="sabilditia/nestra-models",
+        repo_type="model"
+    )
+    print(f"✅ {model} done!")
+
+print("Semua model berhasil diupload!")
